13e448dd5aab46cfaf79c95faa252e94eaebe331
ps9b <154905584+ps9b@users.noreply.github.com>
2025-04-17 12:07:46 +0900

    Update and rename temp.cpp to MouseCallBack.cpp

---
+++ cppfile/MouseCallBack.cpp
@@ -0,0 +1,49 @@
+
+#include<GL/glut.h>
+#include<GL/GL.h>
+#include<GL/GLU.h>
+
+GLint TopLeftX, TopLeftY, BottomRightX, BottomRightY;
+
+void MyDisplay()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 0.0);
+	glBegin(GL_POLYGON);
+		glVertex3f(TopLeftX / 300.0, (300.0 - TopLeftY) / 300.0, 0.0);
+		glVertex3f(TopLeftX / 300.0, (300.0 - BottomRightY) / 300.0, 0.0);
+		glVertex3f(BottomRightX / 300.0, (300.0 - BottomRightY) / 300.0, 0.0);
+		glVertex3f(BottomRightX / 300.0, (300.0 - TopLeftY) / 300.0, 0.0);
+
+	glEnd();
+	glFlush();
+}
+
+void MyMouseClick(GLint Button, GLint State, GLint X, GLint Y) {
+	if (Button == GLUT_LEFT_BUTTON && State == GLUT_DOWN) {
+		TopLeftX = X;
+		TopLeftY = Y;
+	}
+}
+
+void MyMouseMove(GLint X, GLint Y) {
+	BottomRightX = X;
+	BottomRightY = Y;
+	glutPostRedisplay();
+}
+
+int main(int argc, char* argv[])
+{
+	glutInit(&argc, argv);
+	glutInitWindowSize(300, 300);
+	glutCreateWindow("Open Example");
+
+	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+
+	glutDisplayFunc(MyDisplay);
+	glutMouseFunc(MyMouseClick);
+	glutMotionFunc(MyMouseMove);
+
+	glutMainLoop();
+	return 0;
+}
--- cppfile/temp.cpp
+++
@@ -1,6 +0,0 @@
-#include <glut.h>
-#include <gl/GL.h>
-#include <gl/GLU.h>
-void main(){
-
-}
