565e2f9841851814ef12a5cb03fbe61edf0b84a1
ps9b <154905584+ps9b@users.noreply.github.com>
2025-04-22 20:09:16 +0900

    GLUTobject.cpp 업데이트

--- cppfile/GLUTobject.cpp
+++ cppfile/GLUTobject.cpp
@@ -8,11 +8,13 @@ void MyDisplay()
 	glRotatef(-90, 1.0, 0.0, 0.0);
 	
 	glutWireSphere(0.5, 24, 6);
-	/*glutSolidSphere(0.5, 24, 6);
+	/*
+glutSolidSphere(0.5, 24, 6);
 	glutWireCube(1.0);
 	glutWireCone(0.4, 0.8, 18, 9);
 	glutWireTorus(0.2, 0.6, 18, 36);
-	glutWireTeapot(0.5);*/
+	glutWireTeapot(0.5);
+*/
 
 	glFlush();
 }
